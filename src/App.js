3836ce641f987c2cc1c65ee539ebc9f96b434409
Ryan Bourdais <ryanbourdais@outlook.com>
2021-12-29 16:49:00 -0600

    completed course

--- src/App.js
+++ src/App.js
@@ -5,15 +5,35 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+// import {
+//   DECREASE,
+//   INCREASE,
+//   RESET,
+//   CHANGE_NAME,
+//   GET_TOTAL,
+//   GET_AMOUNT,
+// } from "./actions";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
+const store = createStore(reducer, initialStore);
 
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer />
+    </Provider>
   );
 }
 
